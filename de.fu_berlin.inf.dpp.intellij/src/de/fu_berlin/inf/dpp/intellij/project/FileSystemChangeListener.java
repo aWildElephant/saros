01483e950278b5b81e35c22c5c95b19719b4a021
Etienne Girard <etienne.g.girard@gmail.com>
2016-06-01 16:04:37 +0200

    Check if newly created file is a shared resource
    Change-Id: Id84b7765c33d88491188a05f84fd3b9db373571f

--- de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/project/FileSystemChangeListener.java
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/project/FileSystemChangeListener.java
@@ -199,6 +199,11 @@ public class FileSystemChangeListener extends AbstractStoppableListener
         path = makeAbsolutePathProjectRelative(path, project);
 
         SPath spath = new SPath(project, path);
+
+        if (!resourceManager.getSession().isShared(spath.getResource())) {
+            return;
+        }
+
         User user = resourceManager.getSession().getLocalUser();
         IActivity activity;
 
